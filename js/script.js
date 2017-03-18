654e49420b77bfd876dc4f249d18576dbd9e70d5
lafjonn <shebashova.s@gmail.com>
2017-03-18 18:58:49 +0300

    styles corrections, js added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector(".header__dropdown-btn");
+var menu = document.querySelector(".header__menu-list");
+var menuToggle = document.querySelector(".header__menu-list--open");
+
+menu.classList.remove("header__menu-list--open");
+
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (menu.classList.contains("header__menu-list--open")) {
+    menu.classList.remove("header__menu-list--open");
+  } else {
+    menu.classList.add("header__menu-list--open");
+  }
+});
