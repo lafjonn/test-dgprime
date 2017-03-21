d45ec8a8bb57d59fd1e0cdeafae4dac393a199a6
lafjonn <shebashova.s@gmail.com>
2017-03-21 13:10:00 +0300

    menu position corrections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var btn = document.querySelector(".header__dropdown-btn");
 var menu = document.querySelector(".header__menu-list");
 var wrapper = document.querySelector(".header__wrapper");
+var menuAbsolute = document.querySelector(".header__menu");
 
 menu.classList.remove("header__menu-list--open");
 
@@ -10,8 +11,12 @@ btn.addEventListener("click", function (event) {
   if (menu.classList.contains("header__menu-list--open")) {
     menu.classList.remove("header__menu-list--open");
     wrapper.classList.remove("header__wrapper--color");
+    wrapper.classList.add("absolute");
+    menuAbsolute.classList.add("absolute");
   } else {
     menu.classList.add("header__menu-list--open");
     wrapper.classList.add("header__wrapper--color");
+    wrapper.classList.remove("absolute");
+    menuAbsolute.classList.remove("absolute");
   }
 });
